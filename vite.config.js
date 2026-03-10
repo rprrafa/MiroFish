1551346e34640e7a0e19be999f6bcd90c8583a02
Rafael do Prado Ribeiro <rpr.rafa@gmail.com>
2026-03-10 09:38:26 -0300

    Add Vite configuration for Vue and server settings

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    allowedHosts: ['.coolify-dev.startse.com'],
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://api:5001',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+})
